9b537d703d682bdecd549ca8b0af919146493f94
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-10-28 10:08:04 +0800

    feat: handle SIGHUP

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { buildServer } from './server'
 import { PORT, HOST, CI } from '@env'
 
+process.on('SIGHUP', () => process.exit(1))
+
 ;(async () => {
   const server = await buildServer()
   await server.listen(PORT(), HOST())
