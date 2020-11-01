0d30ade1e4b25f2bafcc3432b8d20a6033ebf287
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-11-01 14:54:32 +0800

    fix: logging level

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,12 @@ import fastify from 'fastify'
 import cors from 'fastify-cors'
 import { routes as nanoid } from '@src/services/nanoid'
 import { routes as uuid } from '@src/services/uuid'
-import { HTTP2 } from '@env'
+import { HTTP2, NODE_ENV, NodeEnv } from '@env'
 import Core from '@core'
 
 export function buildServer({ logger = false }: Partial<{ logger: boolean }> = {}) {
   const server = fastify({
-    logger
+    logger: getLoggerOptions()
     /* @ts-ignore */
   , http2: HTTP2()
   })
@@ -15,4 +15,12 @@ export function buildServer({ logger = false }: Partial<{ logger: boolean }> = {
   server.register(nanoid, { Core })
   server.register(uuid, { Core })
   return server
+}
+
+function getLoggerOptions() {
+  switch (NODE_ENV()) {
+    case NodeEnv.Production: return { level: 'error' }
+    case NodeEnv.Development: return { level: 'trace' }
+    default: return false
+  }
 }
