99750917c2a7958c5ade4e616c3a687321c58a85
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-12-12 16:39:58 +0800

    feat: memoize environments

--- src/env.ts
+++ src/env.ts
@@ -1,33 +1,35 @@
+import { memoize } from 'lodash'
+
 export enum NodeEnv {
   Test
 , Development
 , Production
 }
 
-export function NODE_ENV(): NodeEnv | undefined {
+export const NODE_ENV = memoize(function (): NodeEnv | undefined {
   switch (process.env.NODE_ENV) {
     case 'test': return NodeEnv.Test
     case 'development': return NodeEnv.Development
     case 'production': return NodeEnv.Production
   }
-}
+})
 
-export function PORT(): number {
+export const PORT = memoize(function (): number {
   if (process.env.UID_PORT) {
     return Number(process.env.UID_PORT)
   } else {
     return 8080
   }
-}
+})
 
-export function HOST(): string {
+export const HOST = memoize(function (): string {
   return process.env.UID_HOST ?? 'localhost'
-}
+})
 
-export function HTTP2(): boolean {
+export const HTTP2 = memoize(function (): boolean {
   return process.env.UID_HTTP2 === 'true'
-}
+})
 
-export function CI(): boolean {
+export const CI = memoize(function (): boolean {
   return process.env.CI === 'true'
-}
+})
