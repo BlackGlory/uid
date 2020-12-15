0816086f30ab3f192cfecfa234b8d64576516738
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-12-15 16:28:20 +0800

    chore: rename and remove

--- __tests__/services/uid/nanoid.spec.ts
+++ __tests__/services/uid/nanoid.spec.ts
@@ -1,21 +1,7 @@
 import { buildServer } from '@src/server'
-import Core from '@core'
-import { mocked } from 'ts-jest/utils'
-import '@blackglory/jest-matchers'
-
-jest.mock('@core', () => {
-  return {
-    default: {
-      Nanoid: {
-        generate: jest.fn().mockReturnValue('nanoid')
-      }
-    }
-  }
-})
 
 describe('GET /nanoid', () => {
   it('200', async () => {
-    const generate = mocked(Core.Nanoid.generate)
     const server = buildServer()
 
     const res = await server.inject({
@@ -24,7 +10,5 @@ describe('GET /nanoid', () => {
     })
 
     expect(res.statusCode).toBe(200)
-    expect(generate).toBeCalled()
-    expect(res.body).toBeResultOf(generate)
   })
 })
--- __tests__/services/uid/uuid.spec.ts
+++ __tests__/services/uid/uuid.spec.ts
@@ -1,21 +1,7 @@
 import { buildServer } from '@src/server'
-import Core from '@core'
-import { mocked } from 'ts-jest/utils'
-import '@blackglory/jest-matchers'
-
-jest.mock('@core', () => {
-  return {
-    default: {
-      UUID: {
-        generate: jest.fn().mockReturnValue('uuid')
-      }
-    }
-  }
-})
 
 describe('GET /uuid', () => {
   it('200', async () => {
-    const generate = mocked(Core.UUID.generate)
     const server = buildServer()
 
     const res = await server.inject({
@@ -24,7 +10,5 @@ describe('GET /uuid', () => {
     })
 
     expect(res.statusCode).toBe(200)
-    expect(generate).toBeCalled()
-    expect(res.body).toBeResultOf(generate)
   })
 })
