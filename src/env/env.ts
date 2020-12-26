7ff750ea1ca07aeb98b8d7c083c26791841d176a
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-12-27 00:58:50 +0800

    refactor: use value-getter

---
+++ src/env/env.ts
@@ -0,0 +1,62 @@
+import { ValueGetter } from 'value-getter'
+import { Getter, isNumber } from '@blackglory/types'
+import { getCache } from '@env/cache'
+
+export enum NodeEnv {
+  Test
+, Development
+, Production
+}
+
+export const NODE_ENV: Getter<NodeEnv | undefined> =
+  env('NODE_ENV')
+    .convert(val => {
+      switch (val) {
+        case 'test': return NodeEnv.Test
+        case 'development': return NodeEnv.Development
+        case 'production': return NodeEnv.Production
+      }
+    })
+    .memoize(getCache)
+    .get()
+
+export const CI: Getter<boolean> =
+  env('CI')
+    .convert(toBool)
+    .default(false)
+    .memoize(getCache)
+    .get()
+
+export const HOST: Getter<string> =
+  env('UID_HOST')
+    .default('localhost')
+    .memoize(getCache)
+    .get()
+
+export const PORT: Getter<number> =
+  env('UID_PORT')
+    .convert(toInteger)
+    .default(8080)
+    .memoize(getCache)
+    .get()
+
+export const HTTP2: Getter<boolean> =
+  env('UID_HTTP2')
+    .convert(toBool)
+    .default(false)
+    .memoize(getCache)
+    .get()
+
+function env(name: string): ValueGetter<string | undefined> {
+  return new ValueGetter(() => process.env[name])
+}
+
+function toBool(val: string | boolean | undefined): boolean | undefined {
+  if (val) return val === 'true'
+  return false
+}
+
+function toInteger(val: string | number | undefined ): number | undefined {
+  if (isNumber(val)) return val
+  if (val) return Number.parseInt(val, 10)
+}
