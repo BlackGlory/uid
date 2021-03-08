a8090ee4235a49ea404cf517b51208a599bf72a1
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-03-08 03:53:12 +0000

    refactor: use @blackglory/go

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { go } from '@blackglory/go'
 import { buildServer } from './server'
 import { PORT, HOST, CI } from '@env'
 
@@ -5,10 +6,10 @@ process.on('SIGHUP', () => process.exit(128 + 1))
 process.on('SIGINT', () => process.exit(128 + 2))
 process.on('SIGTERM', () => process.exit(128 + 15))
 
-;(async () => {
+go(async () => {
   const server = await buildServer()
   await server.listen(PORT(), HOST())
   if (CI()) await process.exit()
 
   process.send?.('ready')
-})()
+})
