6fc3097cf6dd834d776b4770fdd70b74e971727c
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2022-12-17 11:49:52 +0000

    refactor: remove deprecated lines

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { PORT, HOST, NODE_ENV, NodeEnv } from '@env/index.js'
 
 go(async () => {
   const server = await buildServer()
-  await server.listen(PORT(), HOST())
+  await server.listen({ port: PORT(), host: HOST() })
   if (NODE_ENV() === NodeEnv.Test) process.exit()
 
   process.send?.('ready')
